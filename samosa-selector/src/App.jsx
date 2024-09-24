93fb003a59c1264a119b426aaed587523dcb1de6
paul-leiva <54649136+paul-leiva@users.noreply.github.com>
2024-09-24 19:44:34 -0400

    Step 1

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -4,6 +4,11 @@ const App = () => {
 
   return (
     <div className="App">
+      <div className="header">
+        <h1>Samosa Selector</h1>
+        <h2>Count:</h2>
+        <img className="samosa" src="https://helpful-griffin-0a0ba5.netlify.app/samosa.png"></img>
+      </div>
 
     </div>
   )
