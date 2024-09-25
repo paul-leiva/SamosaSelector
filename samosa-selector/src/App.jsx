4ec38a5d9803d9183617e8fd6aafca987babd811
paul-leiva <54649136+paul-leiva@users.noreply.github.com>
2024-09-24 20:35:17 -0400

    Real Step 3 (Add upgrades)

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -6,13 +6,33 @@ const App = () => {
   const [count, setCount] = useState(0);
   const [multiplier, setMultiplier] = useState(1);
   const updateCount = () => setCount(count + multiplier);
-  
+
   return (
     <div className="App">
       <div className="header">
         <h1>Samosa Selector</h1>
         <h2>Count: {count}</h2>
         <img className="samosa" src="https://helpful-griffin-0a0ba5.netlify.app/samosa.png" onClick={updateCount}></img>
+      </div>
+
+      <div className="container">
+        <div className="upgrade">
+          <h3>Double Stuffed 👯‍♀️</h3>
+          <p>2x per click</p>
+          <button>10 samosas</button>
+        </div>
+
+        <div className="upgrade">
+          <h3>Party Pack 🎉</h3>
+          <p>5x per click</p>
+          <button>100 samosas</button>
+        </div>
+
+        <div className="upgrade">
+          <h3>Full Feast 👩🏽‍🍳</h3>
+          <p>10x per click</p>
+          <button>1000 samosas</button>
+        </div>
       </div>
 
     </div>
